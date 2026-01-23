53d08ac52580ccaaef42b29785ac4032490f21bd
John Felts <john@creative2.com>
2026-01-23 10:18:37 -0500

    Stats

---
+++ blocks/statistics/statistics.js
@@ -0,0 +1,41 @@
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+function buildItem(row) {
+  const item = document.createElement('li');
+  item.className = 'statistics-item';
+  moveInstrumentation(row, item);
+
+  const cells = [...row.children];
+  const valueCell = cells[0];
+  const labelCell = cells[1];
+
+  if (valueCell) {
+    const value = document.createElement('div');
+    value.className = 'statistics-value';
+    moveInstrumentation(valueCell, value);
+    while (valueCell.firstChild) value.append(valueCell.firstChild);
+    if (value.childNodes.length) item.append(value);
+  }
+
+  if (labelCell) {
+    const label = document.createElement('div');
+    label.className = 'statistics-label';
+    moveInstrumentation(labelCell, label);
+    while (labelCell.firstChild) label.append(labelCell.firstChild);
+    if (label.childNodes.length) item.append(label);
+  }
+
+  return item;
+}
+
+export default function decorate(block) {
+  const list = document.createElement('ul');
+  list.className = 'statistics-list';
+
+  [...block.children].forEach((row) => {
+    const item = buildItem(row);
+    if (item.childNodes.length) list.append(item);
+  });
+
+  block.replaceChildren(list);
+}
