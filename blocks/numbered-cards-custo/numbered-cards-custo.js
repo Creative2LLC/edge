705f887a9a6e63a3c9a30e6f2e041679ffdf7793
JP <jp@creative2.com>
2026-03-24 17:05:12 -0400

    Fixed css and js to custo instead of custom since it makes it go over 20 characters for the block name

---
+++ blocks/numbered-cards-custo/numbered-cards-custo.js
@@ -0,0 +1,5 @@
+import decorateNumberedCardsCustom from '../numbered-cards-custom/numbered-cards-custom.js';
+
+export default function decorate(block) {
+  return decorateNumberedCardsCustom(block);
+}
