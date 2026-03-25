f7a8dc00443eea66fcc481d24414bf363355119f
Brendan Shulman <brendanshulman@Not-a-Mac-mini.local>
2026-03-25 17:12:01 -0400

    lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,7 @@ module.exports = {
       'leadership-profile': 8,
       'cta-card-1': 9,
       'cta-card-2': 8,
-      'info-card': 10,
+      'info-card': 11,
       'card-row-item': 9,
       'card-row-detailed-item': 14,
       'split-card': 11,
