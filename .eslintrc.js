354b214a04cd3c12722239dc7cbdc9f7e9d1a8f7
Brendan Shulman <brendanshulman@Not-a-Mac-mini.local>
2026-04-30 10:04:13 -0400

    lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,7 @@ module.exports = {
       'impact-chain-item': 5,
       'leader-card': 7,
       'leadership-profile': 8,
-      'cta-card-1': 14,
+      'cta-card-1': 15,
       'cta-card-2': 8,
       'info-card': 18,
       'card-row-item': 9,
