8dda729ef029662dc8fe23d90d4fd4a0c826462f
JP <jp@creative2.com>
2026-05-19 12:11:33 -0400

    Tweaks

--- blocks/article-details/article-details.js
+++ blocks/article-details/article-details.js
@@ -76,6 +76,10 @@ function decodeBase64Utf8(value) {
   }
 }
 
+function hasEmbeddedImage(value) {
+  return /<(img|picture|source)\b/i.test(`${value || ''}`);
+}
+
 function readTextValue(block, name) {
   const namedText = readTextField(block, name).value;
   if (namedText) return namedText;
@@ -324,6 +328,16 @@ function buildHero(fields) {
 function chooseArticleBody(block, resourceData) {
   const richTextBody = getHtmlField(block, 'articleBody');
   const rawBody = normalizeRawHtmlField(block, resourceData);
+
+  if (
+    rawBody
+    && (!richTextBody.html || (hasEmbeddedImage(rawBody) && !hasEmbeddedImage(richTextBody.html)))
+  ) {
+    return {
+      html: rawBody,
+      source: richTextBody.source,
+    };
+  }
 
   return richTextBody.html ? richTextBody : {
     html: rawBody,
