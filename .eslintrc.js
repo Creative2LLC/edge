771bdddef071750d6c1a8bb12a64ab0c6eb292b4
Brendan Shulman <brendanshulman@Not-a-Mac-mini.local>
2026-05-20 10:31:21 -0400

    hero button style dropdown updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
     'xwalk/max-cells': ['error', {
-      '*': 13,
+      '*': 16,
       'news-article': 5,
       resources: 12,
       'resource-item': 7,
